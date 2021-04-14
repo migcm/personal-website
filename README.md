e9fc94bbe2a51b3bba730ddeba9e9fd0d5483600
Miguel Calvo Matalobos <26927839+migcm@users.noreply.github.com>
2021-04-14 13:24:04 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# personal-website
+
+This is my personal website, where I post my projects and my social networks.
+
+miguelcm.es
---
+++ assets/js/scripts.js
@@ -0,0 +1,71 @@
+/*####################### Filter Buttons ####################################### */
+
+filterSelection("all")
+
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+
+/*####################### Filter Suche ####################################### */
+
+function searchFunction() {
+    // Declare variables
+    var input, filter, list, i;
+    input = document.getElementById('searchinput');
+    filter = input.value.toUpperCase();
+    list = document.getElementsByClassName('content');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < list.length; i++) {
+        if (list[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            list[i].parentElement.parentElement.style.display = "";
+        } else {
+            list[i].parentElement.parentElement.style.display = "none";
+        }
+    }
+}
+
+
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function(){
+    var div = this.parentElement;
+    div.style.opacity = "0";
+    setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
+}
